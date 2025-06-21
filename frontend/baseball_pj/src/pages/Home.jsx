798e5533d0ead4aaa8408611d602523e197f38fc
python_pyb <dbqls3698@naver.com>
2025-06-21 12:52:53 +0900

    VOTE 기능 구현중

--- frontend/baseball_pj/src/pages/Home.jsx
+++ frontend/baseball_pj/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 // Home 컴포넌트: 메인 페이지(홈) 역할, 추후 컨텐츠 추가 가능
 import { Link } from 'react-router-dom';
+import VoteGraph from "./VoteGraph";
+import "./Home.css";
 
 function Home() {
   return (
@@ -8,6 +10,8 @@ function Home() {
       <ul>
         {/* 추후 홈 컨텐츠 추가 */}
       </ul>
+      {/* 오른쪽 하단에 실시간 투표현황 그래프 */}
+      <VoteGraph />
     </div>
   );
 }
