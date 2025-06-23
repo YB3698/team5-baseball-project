7f91ede77df346d84539941825d8406fdec84b42
ManiacMemories <113078344+ManiacMemories@users.noreply.github.com>
2025-06-23 17:57:22 +0900

    comments add
    댓글,대댓글 기능 추가

--- frontend/baseball_pj/src/pages/PostForm.jsx
+++ frontend/baseball_pj/src/pages/PostForm.jsx
@@ -28,7 +28,7 @@ const PostForm = () => {
   const [submitted, setSubmitted] = useState(false);
   const navigate = useNavigate();
 
-  // 사용자 정보 가져오기 (token 제거)
+  // 사용자 정보 가져오기
   let user = null;
   let userId = null;
   try {
@@ -55,7 +55,7 @@ const PostForm = () => {
         teamId,
         postTitle: title,
         postContent: content
-      }); // ✅ Authorization 헤더 제거
+      }); 
 
       setSubmitted(true);
     } catch (err) {
